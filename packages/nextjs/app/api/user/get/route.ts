12fe6c08f493d8bc58357be18c8cac84cc82891e
sandyagur8 <santiago.appu@gmail.com>
2024-09-04 21:48:35 +0530

    added fix for duplicate user

--- packages/nextjs/app/api/user/get/route.ts
+++ packages/nextjs/app/api/user/get/route.ts
@@ -3,7 +3,6 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { supabase } from "../../../lib/supabase";
 
-
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const email = searchParams.get("email");
