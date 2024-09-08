a7ac64ef2db2f8b5d19c287efab86df58c40c50c
Anirudh <ukanirudh@gmail.com>
2024-09-08 15:48:14 +0530

    user page changed

--- packages/nextjs/app/signup/page.tsx
+++ packages/nextjs/app/signup/page.tsx
@@ -4,7 +4,7 @@ import SignUpForm from "../../components/SignUpForm";
 
 export default function SignUp() {
   return (
-    <div className="min-h-screen flex flex-col bg-gradient-to-br from-blue-500 to-purple-600">
+    <div className="min-h-screen flex flex-col bg-[#001f3d] text-black">
       <main className="flex-grow container mx-auto px-4 py-8">
         <div className="max-w-md mx-auto">
           <SignUpForm />
--- packages/nextjs/app/verify-otp/page.tsx
+++ packages/nextjs/app/verify-otp/page.tsx
@@ -127,18 +127,18 @@ function VerifyOTPContent() {
       functionName: "approve",
       args: [LOAN_ADDRESS, parseEther("10000000")],
     });
-    // await kintoSDK.sendTransaction([
-    //   { to: `0x${USDC_ADDRESS.slice(2)}`, data, value: BigInt(0) }])
-    //   await kintoSDK.sendTransaction([
-    //   { to: `0x${LOAN_ADDRESS.slice(2)}`, data: data2, value: BigInt(0) }])
-    //   await kintoSDK.sendTransaction([
-    //   { to: `0x${USDC_ADDRESS.slice(2)}`, data:data3, value: BigInt(0) }]) // uncomment if needed
-
     await kintoSDK.sendTransaction([
-      { to: `0x${USDC_ADDRESS.slice(2)}`, data, value: BigInt(0) },
-      { to: `0x${LOAN_ADDRESS.slice(2)}`, data: data2, value: BigInt(0) },
-      { to: `0x${USDC_ADDRESS.slice(2)}`, data: data3, value: BigInt(0) },
-    ]);
+      { to: `0x${USDC_ADDRESS.slice(2)}`, data, value: BigInt(0) }])
+      await kintoSDK.sendTransaction([
+      { to: `0x${LOAN_ADDRESS.slice(2)}`, data: data2, value: BigInt(0) }])
+      await kintoSDK.sendTransaction([
+      { to: `0x${USDC_ADDRESS.slice(2)}`, data:data3, value: BigInt(0) }]) // uncomment if needed
+
+    // await kintoSDK.sendTransaction([
+    //   { to: `0x${USDC_ADDRESS.slice(2)}`, data, value: BigInt(0) },
+    //   { to: `0x${LOAN_ADDRESS.slice(2)}`, data: data2, value: BigInt(0) },
+    //   { to: `0x${USDC_ADDRESS.slice(2)}`, data: data3, value: BigInt(0) },
+    // ]);
     //getting necessary approvals and creating the user on the loan contract
     setShowSuccessModal(false);
 
